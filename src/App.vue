64657fe0af74d1ce9697d197cb5c7106a50ae5bc
Teeradet <tee2490@hotmail.com>
2025-09-10 05:58:02 +0700

    18 header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
+<template>
+  <div class="d-flex flex-column py-2 min-vh-100 container-md">
+    <Header />
+    <main class="flex-grow-1 mx-lg-auto w-100">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
+</template>
+
 <script setup>
+import Header from './components/layout/Header.vue'
+import Footer from './components/layout/Footer.vue'
 import { RouterLink, RouterView } from 'vue-router'
 </script>
-
-<template>
-  <RouterView />
-</template>
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div
+      class="footer text-center mx-auto mt-4 p-3 bg-opacity-10 text-success bg-success rounded-3"
+    >
+      &copy; Mady with <i class="bi bi-heart-fill"></i> by DotNetMastery
+    </div>
+  </div>
+</template>
