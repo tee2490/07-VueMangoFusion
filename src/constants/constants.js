d514bd8744450726ca2004371985163723bd1158
Teeradet <tee2490@hotmail.com>
2025-09-10 05:49:04 +0700

    18 define routes

---
+++ src/constants/constants.js
@@ -0,0 +1,22 @@
+export const CATEGROIES = ['Appetizer', 'Entrée', 'Dessert']
+
+export const SORT_PRICE_LOW_HIGH = 'PRICE LOW-HIGH'
+export const SORT_PRICE_HIGH_LOW = 'PRICE HIGH-LOW'
+export const SORT_NAME_A_Z = 'NAME A-Z'
+export const SORT_NAME_Z_A = 'NAME Z-A'
+
+export const SORT_OPTIONS = [SORT_NAME_A_Z, SORT_NAME_Z_A, SORT_PRICE_LOW_HIGH, SORT_PRICE_HIGH_LOW]
+
+export const ROLES = ['Admin', 'Customer']
+
+export const ORDER_STATUS_CONFIRMED = 'Confirmed'
+export const ORDER_STATUS_READY_FOR_PICKUP = 'Ready for Pickup'
+export const ORDER_STATUS_COMPLETED = 'Completed'
+export const ORDER_STATUS_CANCELLED = 'Cancelled'
+
+export const ORDER_STATUS = [
+  ORDER_STATUS_CONFIRMED,
+  ORDER_STATUS_READY_FOR_PICKUP,
+  ORDER_STATUS_COMPLETED,
+  ORDER_STATUS_CANCELLED,
+]
