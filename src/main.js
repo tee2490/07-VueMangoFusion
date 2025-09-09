471e1cdc392609a3adc5a055cdab5984b269058d
Teeradet <tee2490@hotmail.com>
2025-09-10 06:25:11 +0700

    18 pinia persist

--- src/main.js
+++ src/main.js
@@ -5,10 +5,20 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App.vue'
 import router from './router/routes'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { useThemeStore } from './stores/themeStore'
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+
 app.use(router)
+
+const themeStore = useThemeStore()
+if (themeStore.theme) {
+  document.body.setAttribute('data-bs-theme', themeStore.theme)
+}
 
 app.mount('#app')
--- src/stores/themeStore.js
+++ src/stores/themeStore.js
@@ -1,15 +1,19 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useThemeStore = defineStore('themeStore', () => {
-  const theme = ref('dark')
+export const useThemeStore = defineStore(
+  'themeStore',
+  () => {
+    const theme = ref('dark')
 
-  const setTheme = (newTheme) => {
-    theme.value = newTheme
-    document.body.setAttribute('data-bs-theme', newTheme)
-  }
-  return {
-    theme,
-    setTheme,
-  }
-})
+    const setTheme = (newTheme) => {
+      theme.value = newTheme
+      document.body.setAttribute('data-bs-theme', newTheme)
+    }
+    return {
+      theme,
+      setTheme,
+    }
+  },
+  { persist: true },
+)
