0d3ba08e62fb39bc5caaa5f0b8aa207ee03dfa26
Teeradet <tee2490@hotmail.com>
2025-09-10 05:13:31 +0700

    18 clean project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from './router/routes'
 
 const app = createApp(App)
 
