397e08b49191aa8c91ede863dcf887da6aa71337
Teeradet <tee2490@hotmail.com>
2025-09-10 20:29:31 +0700

    21 sign up and sign in validations

--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -1,1 +1,82 @@
-<template>Sign In</template>
+<template>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6 col-lg-4">
+        <div class="card shadow">
+          <div class="card-body p-4">
+            <h2 class="text-center mb-4">Sign In</h2>
+            <form @submit.prevent="onSignInSubmit">
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" v-model="formObj.email" class="form-control" id="email" />
+              </div>
+
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input
+                  type="password"
+                  v-model="formObj.password"
+                  class="form-control"
+                  id="password"
+                />
+              </div>
+
+              <div class="alert alert-danger" v-if="errorList.length > 0">
+                <span class="d-block" v-for="error in errorList" :key="error"> {{ error }} </span>
+              </div>
+
+              <button :disabled="isLoading" type="submit" class="btn btn-secondary w-100">
+                <span v-if="isLoading" class="spinner-border spinner-border-sm me-2"></span>
+                Login
+              </button>
+            </form>
+
+            <div class="text-center mt-3">
+              <router-link :to="{ name: APP_ROUTE_NAMES.SIGN_UP }"
+                >Don't have an account? Sign up</router-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ROLES } from '@/constants/constants'
+import { APP_ROUTE_NAMES } from '@/constants/routeNames'
+import { reactive, ref } from 'vue'
+const formObj = reactive({
+  email: '',
+  password: '',
+})
+
+const isLoading = ref(false)
+
+const errorList = reactive([])
+
+const onSignInSubmit = async () => {
+  isLoading.value = true
+  errorList.length = 0
+  console.log(formObj)
+  if (formObj.email === undefined || formObj.email.length === 0) {
+    errorList.push('Email is required.')
+  }
+
+  if (formObj.password === undefined || formObj.password.length === 0) {
+    errorList.push('Password is required.')
+  }
+  if (errorList.length > 0) {
+    isLoading.value = false
+    return
+  }
+
+  try {
+  } catch (err) {
+    errorList.push(err)
+  } finally {
+    isLoading.value = false
+  }
+}
+</script>
--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -1,1 +1,99 @@
-<template>Sign Up</template>
+<template>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6 col-lg-4">
+        <div class="card shadow">
+          <div class="card-body p-4">
+            <h2 class="text-center mb-4">Sign Up</h2>
+
+            <form @submit.prevent="onSignUpSubmit">
+              <div class="mb-3">
+                <label for="name" class="form-label">Full Name</label>
+                <input type="text" class="form-control" id="name" v-model="formObj.name" />
+              </div>
+
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" v-model="formObj.email" />
+              </div>
+
+              <div class="mb-3">
+                <label for="role" class="form-label">Role</label>
+                <select class="form-select" id="role" v-model="formObj.role">
+                  <option v-for="role in ROLES" :key="role">{{ role }}</option>
+                </select>
+              </div>
+
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  v-model="formObj.password"
+                  id="password"
+                />
+              </div>
+
+              <div class="alert alert-danger" v-if="errorList.length > 0">
+                <span class="d-block" v-for="error in errorList" :key="error"> {{ error }} </span>
+              </div>
+
+              <button :disabled="isLoading" type="submit" class="btn btn-secondary w-100">
+                <span v-if="isLoading" class="spinner-border spinner-border-sm me-2"></span>
+                Sign Up
+              </button>
+            </form>
+
+            <div class="text-center mt-3">
+              <router-link :to="{ name: APP_ROUTE_NAMES.SIGN_IN }"
+                >Already have an account? Login</router-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ROLES } from '@/constants/constants'
+import { APP_ROUTE_NAMES } from '@/constants/routeNames'
+import { reactive, ref } from 'vue'
+const formObj = reactive({
+  name: '',
+  email: '',
+  password: '',
+  role: 'Customer',
+})
+
+const isLoading = ref(false)
+
+const errorList = reactive([])
+
+const onSignUpSubmit = async () => {
+  isLoading.value = true
+  errorList.length = 0
+  console.log(formObj)
+  if (formObj.name === undefined || formObj.name.length === 0) {
+    errorList.push('Name is required.')
+  }
+  if (formObj.email === undefined || formObj.email.length === 0) {
+    errorList.push('Email is required.')
+  }
+  if (formObj.password === undefined || formObj.password.length === 0) {
+    errorList.push('Password is required.')
+  }
+  if (errorList.length > 0) {
+    isLoading.value = false
+    return
+  }
+
+  try {
+  } catch (err) {
+    errorList.push(err)
+  } finally {
+    isLoading.value = false
+  }
+}
+</script>
