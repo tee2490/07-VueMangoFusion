b233b8684b8faf20160fca608c314db383022bb0
Teeradet <tee2490@hotmail.com>
2025-09-11 20:03:31 +0700

    23 cursor-pointer readonly in rating

--- src/components/shared/Rating.vue
+++ src/components/shared/Rating.vue
@@ -2,21 +2,14 @@
   <div class="d-flex align-items-center">
     <small class="text-secondary me-2" v-if="!readonly">Rate this item:</small>
     <div class="d-flex">
-      <div v-for="star in 5" :key="star" class="star-rating me-1" @click="onRatingUpdate(star)">
-        <i
-          :class="rating && rating >= star ? 'bi-star-fill' : 'bi-star'"
-          class="text-warning"
-          width="16"
-        ></i>
+      <div v-for="star in 5" :key="star" class="star-rating me-1" @click="!readonly && onRatingUpdate(star)"
+        :class="{ 'cursor-pointer': !readonly }">
+        <i :class="rating && rating >= star ? 'bi-star-fill' : 'bi-star'" class="text-warning" width="16"></i>
       </div>
     </div>
 
     <div v-if="rating" class="ms-2 text-secondary small">({{ rating }})</div>
-    <div
-      v-if="isProcessing"
-      class="spinner-border spinner-border-sm text-warning ms-2"
-      role="status"
-    >
+    <div v-if="isProcessing" class="spinner-border spinner-border-sm text-warning ms-2" role="status">
       <span class="visually-hidden">Rating...</span>
     </div>
     <div class="text-success ms-2" v-if="isRatingSuccessful">
@@ -39,6 +32,10 @@ const props = defineProps({
   rating: {
     type: [Number],
     default: 0,
+  },
+  readonly: {
+    type: Boolean,
+    default: false,
   },
 })
 const onRatingUpdate = async (newRating) => {
