16b300f051f6ffb2a82bb18deac235d0e156e058
Teeradet <tee2490@hotmail.com>
2025-09-11 19:04:02 +0700

    23 pass token to API

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,20 @@
 import axios from 'axios'
 import { CONFIG_API_URL } from '@/constants/config'
+import Cookies from 'js-cookie'
 
 const api = axios.create({
   baseURL: CONFIG_API_URL,
 })
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('token_mango')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error),
+)
 
 export default api
